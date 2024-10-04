a26105f3a65172378392d52a8ad086544ccf636e
NoNotNotes <sevenrieman@gmail.com>
2024-10-05 06:21:22 +0800

    modified

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "hsterts.github.io/pc-doctor",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
+    defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
